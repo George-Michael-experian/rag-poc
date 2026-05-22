660de154182eb2339cfdf344ab7786d240620e77
George-Michael-experian <George-michael.kondoun@experian.com>
2026-05-21 23:54:36 -0500

    updated vector.py

--- vectordb.py
+++ vectordb.py
@@ -1,7 +1,25 @@
 import chromadb
 
-client = chromadb.client()
+client = chromadb.Client()
 
 collection = client.create_collection("policies")
 
+#add 3 policy documents (Chroma handles embeddings autimatically)
 
+policies = [
+    "Dogs are allowed in the office on Fridays",
+    "Pets can come to work on Furry Fridays",
+    "Remote work policy allows 3 days from home"
+]
+ 
+for i, policy in enumerate(policies):
+    collection.add(
+        documents = [policy],
+        ids = [f"policy_{i}"]
+    )
+
+#query the system
+query = "Can I bring my dog to work?"
+results = collection.query(query_texts= [query], n_results=2)
+
+print(results)
